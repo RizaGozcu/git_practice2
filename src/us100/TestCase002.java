fde25e48f581f0eec1aec0beac7076e313786f0a
yelcan <70501815+yelcan@users.noreply.github.com>
2022-01-11 11:54:29 -0500

    step 3 done

--- src/us100/TestCase002.java
+++ src/us100/TestCase002.java
@@ -1,6 +1,9 @@
 package us100;
 
 public class TestCase002 {
+    public static void main(String[] args) {
+        System.out.println("case 2");
+    }
     // step 1 done
     // step 2 done
     // step 3 done
